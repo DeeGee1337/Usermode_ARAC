f75d8b84cccd011eb22dd09a3b16ebfca96de7d4
DeeGee1337 <53494893+DeeGee1337@users.noreply.github.com>
2022-04-11 18:45:14 +0200

    TOPMOST & AERO Overlaydetection

--- ACAR/ACAR/windowtitles.hpp
+++ ACAR/ACAR/windowtitles.hpp
@@ -2,7 +2,7 @@
 
 #include "globals.hpp"
 
-BOOL CALLBACK safe_window(HWND hwnd, LPARAM lParam) 
+BOOL CALLBACK safe_window_topmost(HWND hwnd, LPARAM lParam) 
 {
     const DWORD TITLE_SIZE = 1024;
     WCHAR windowTitle[TITLE_SIZE];
@@ -12,9 +12,37 @@ BOOL CALLBACK safe_window(HWND hwnd, LPARAM lParam)
     int length = ::GetWindowTextLength(hwnd);
     std::wstring title(&windowTitle[0]);
 
-    if (!IsWindowVisible(hwnd) || length == 0 || title == L"Program Manager") 
+    LONG exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+
+    if (exStyle & WS_EX_TOPMOST && (exStyle & WS_EX_TRANSPARENT || exStyle & WS_EX_LAYERED))
     {
-        return TRUE;
+        return TRUE; //topmost and aero + transparent
+    }
+
+    // Retrieve the pointer passed into this callback, and re-'type' it.
+    // The only way for a C API to pass arbitrary data is by means of a void*.
+    std::vector<std::wstring>& titles =
+        *reinterpret_cast<std::vector<std::wstring>*>(lParam);
+    titles.push_back(title);
+
+    return TRUE;
+}
+
+BOOL CALLBACK safe_window(HWND hwnd, LPARAM lParam)
+{
+    const DWORD TITLE_SIZE = 1024;
+    WCHAR windowTitle[TITLE_SIZE];
+
+    GetWindowTextW(hwnd, windowTitle, TITLE_SIZE);
+
+    int length = ::GetWindowTextLength(hwnd);
+    std::wstring title(&windowTitle[0]);
+
+    LONG exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+
+    if (!IsWindowVisible(hwnd) || length == 0 || title == L"Program Manager")
+    {
+       return TRUE; //all open wiondows
     }
 
     // Retrieve the pointer passed into this callback, and re-'type' it.
@@ -28,6 +56,7 @@ BOOL CALLBACK safe_window(HWND hwnd, LPARAM lParam)
 
 void get_open_window_titles()
 {
+    std::wcout << "[WINDOW] Grabbing all open titles" << std::endl;
     std::vector<std::wstring> titles;
     EnumWindows(safe_window, reinterpret_cast<LPARAM>(&titles));
     // At this point, titles if fully populated and could be displayed, e.g.:
@@ -35,5 +64,20 @@ void get_open_window_titles()
     for (const auto& title : titles)
         std::wcout << "[WINDOW] " << L"Title: " << title << std::endl;
    
+    printf("\n");
+
+    std::wcout << "[WINDOW] Grabbing topmost and aero overlays" << std::endl;
+    std::vector<std::wstring> topmosttitles;
+    EnumWindows(safe_window_topmost, reinterpret_cast<LPARAM>(&topmosttitles));
+
+    printf("\n");
+    for (const auto& title : topmosttitles)
+    {
+        if (!title.empty())
+        {
+            std::wcout << "[WINDOW TOPMOST/AERO] " << L"Title: " << title << std::endl;
+        }
+    }
+
     printf("\n");
 }
