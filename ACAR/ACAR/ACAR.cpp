914fad288fac8833097e336ac5019cd9701829ef
ChrisSoGit <raylzimon@gmail.com>
2022-06-13 15:47:44 +0200

    Update ACAR.cpp
    ACAR changes

--- ACAR/ACAR/ACAR.cpp
+++ ACAR/ACAR/ACAR.cpp
@@ -7,8 +7,11 @@ bool t2running = false;
 bool t3running = false;
 bool t4running = false;
 bool t5running = false;
+bool restart_loop = true;
+std::mutex m_lock;
 
-std::vector<sdk::process> processes_hits; //Für was?
+
+std::vector<sdk::process> processes_hits;
 
 //thread1
 std::wstring input;
@@ -25,7 +28,7 @@ void Process_loop()
     processes.push_back(L"cheat");
     processes.push_back(L"ida");
     processes.push_back(L"reclass");
-
+    std::cout << "Thread 1 Start" << std::endl;
     while (t1running == true)
     {
         //loop the vector
@@ -42,12 +45,14 @@ void Process_loop()
             }
         }
         auto start = std::chrono::high_resolution_clock::now();
+        m_lock.lock();
         t1running = false;
         t2running = true;
-        //system("CLS");
+        m_lock.unlock();
+        //system("CLS");       
     }
 }
- 
+
 //Thread 2
 void Get_open_window_titles_loop()
 {
@@ -56,17 +61,19 @@ void Get_open_window_titles_loop()
     while (t2running == true)
     {
         get_open_window_titles();
+        m_lock.lock();
         t2running = false;
         t3running = true;
-        Sleep(10000);
+        m_lock.unlock();
     }
 }
 
-//Thred 3
+//Thread 3
 void Is_protected_game_open_and_killable()
 {
     while (t3running == true)
     {
+
         auto processid = find_processId(game);
 
         if (processid)
@@ -78,17 +85,18 @@ void Is_protected_game_open_and_killable()
                 kill_process_by_ID(processid);
             }
         }
+        m_lock.lock();
         t3running = false;
         t4running = true;
-        Sleep(5000);
+        m_lock.unlock();
+
     }
-    t4running = true;
 }
 
 //Thread 4
-void Search_window(sdk::host h) 
+void Search_window(sdk::host& h)
 {
-    while(t4running == true)
+    while (t4running == true)
     {
         margins window = find_game_window_and_resolution(L"csgo.exe");
 
@@ -103,32 +111,60 @@ void Search_window(sdk::host h)
 
         std::cout << "[MAIN OCR] hostflag: " << (int)h.get_flag() << std::endl;
         */
+        m_lock.lock();
         t4running = false;
-        Sleep(1000);
+        m_lock.unlock();
+
     }
-    t5running = true;
+    h.set_flag(sdk::flags::NONE);
+    if ((int)h.get_flag() <= 3)
+    {
+        m_lock.lock();
+        restart_loop = true;
+        t1running = true;
+        std::this_thread::sleep_for((std::chrono::seconds(5))); //Time until execution restarts with thread1
+        m_lock.unlock();
+    }
+    if ((int)h.get_flag() > 3) {
+        m_lock.lock();
+        restart_loop = false;
+        t5running = true;
+        m_lock.unlock();
+    }
+
 }
 
 //Thread 5
-void Black_list(sdk::host h, WmiQueryResult res, std::string harddrive)
+void Black_list(sdk::host& h, WmiQueryResult res, std::string harddrive)
 {
-    while (t5running == true) 
+    while (t5running == true)
     {
 
         for (auto item : sdk::processes_tuple)
         {
             if ((int)h.get_flag() > 3)
             {
-            
+                m_lock.lock();
                 write_file(create_flagged_hwid_string(sdk::wstring_to_string(res.ResultList.at(0)), harddrive));
+                m_lock.unlock();
                 std::cout << "[MAIN] Added Hardware to Blacklist" << std::endl;
                 std::cout << "[BAN] Found Memorydetection" << std::endl;
                 kill_process_by_ID(std::get<1>(item));
+
             }
         }
+        m_lock.lock();
+        t1running = false;
+        t2running = false;
+        t3running = false;
+        t4running = false;
+        t5running = false;
+        m_lock.unlock();
+
     }
-    t5running = false;
-    t1running = true;
+    //Terminate programm and game??
+
+
 }
 
 
@@ -161,8 +197,8 @@ int main()
     // Get id of CPU
     std::cout << "[HWID] CPU ID: " << std::endl;
     WmiQueryResult CPU = queryAndPrintResult(L"SELECT ProcessorId FROM Win32_Processor", L"ProcessorId");
-    
-  
+
+
 
     for (const auto& item : CPU.ResultList)
         std::wcout << item << std::endl;
@@ -180,7 +216,7 @@ int main()
         hardrives = hardrives.append(sdk::wstring_to_string(item) + " ");
         hard_drives.push_back(sdk::wstring_to_string(item));
     }
-
+    Sleep(10);
     //HWID CHECK HOST
     //open file check for cpu and harddrive IDs
     //if true then terminate and close anything and check other hardwareids if not listed append to blacklist
@@ -195,68 +231,76 @@ int main()
         return 0;
     }
     /*Mutex
-    
+
     std::mutex lock_write_mutex;
     lock_write_mutex.lock();
 
     */
     //Pausesetting for Threads
     auto start = std::chrono::high_resolution_clock::now();
-    
-    
+
+
     //Threads
-    std::thread t1(Process_loop);
-    std::thread t2(Get_open_window_titles_loop); //noch buggy führt bei mir nicht alles aus, printed iwas unsichtbares
-    std::thread t3(Is_protected_game_open_and_killable); //funzt
-    std::thread t4(Search_window,host_object);
-    std::thread t5(Black_list,host_object, CPU, hardrives);
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
-   
-    
-    //Hackerconsole
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, 10); //10 is green
 
-    //
-    sdk::process object(L"Cheat Engine");
 
-    std::cout << "[MEMORY] Searching for strings in processmemory:" << std::endl;
-    auto memresults = 0;
-    auto moduleresults = 0;
-
-    for (auto itt : search_file())
+    while (restart_loop == true)
     {
-        std::cout << "[MEMORY] Searching for \"" << itt << "\"" << std::endl;
-        memresults = memory::search_for_string(object.get_id(), itt.data(), itt.length());
-        std::cout << "[MEMORY] Found " << std::dec << memresults << " references." << std::endl << std::endl;
+        std::thread t1(Process_loop);
+        std::thread t2(Get_open_window_titles_loop);
+        std::thread t3(Is_protected_game_open_and_killable);
+        std::thread t4(Search_window, std::ref(host_object));
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        if (restart_loop == false) {
+            std::thread t5(Black_list, std::ref(host_object), CPU, hardrives);
+            t5.join();
+        }
+
+
+
+
+        //Hackerconsole
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, 10); //10 is green
+
+        //
+        sdk::process object(L"Cheat Engine");
+
+        std::cout << "[MEMORY] Searching for strings in processmemory:" << std::endl;
+        auto memresults = 0;
+        auto moduleresults = 0;
+
+        for (auto itt : search_file())
+        {
+            std::cout << "[MEMORY] Searching for \"" << itt << "\"" << std::endl;
+            memresults = memory::search_for_string(object.get_id(), itt.data(), itt.length());
+            std::cout << "[MEMORY] Found " << std::dec << memresults << " references." << std::endl << std::endl;
+        }
+
+        if (memresults)
+            host_object.set_flag(sdk::flags::Memory);
+
+        std::cout << "[MEMORY] Searching loaded DLL's" << std::endl;
+        moduleresults = memory::detect_modules(object.get_id());
+        std::cout << "[MEMORY] Found " << std::dec << moduleresults << " references." << std::endl << std::endl;
+
+        //if (moduleresults)
+          // host_object.set_flag(sdk::flags::Module);
+
+        ////Can be removed just testing...
+
+
+
+        //Test for attaching CE
+       // debug_string_detection();
+
     }
 
-    if (memresults)
-        host_object.set_flag(sdk::flags::Memory);
-
-    std::cout << "[MEMORY] Searching loaded DLL's" << std::endl;
-    moduleresults = memory::detect_modules(object.get_id());
-    std::cout << "[MEMORY] Found " << std::dec << moduleresults << " references." << std::endl << std::endl;
-
-    if (moduleresults)
-        host_object.set_flag(sdk::flags::Module);
-
-    ////Can be removed just testing...
-    Sleep(5000);
-   
-
-    //Test for attaching CE
-    debug_string_detection();
-
-
-  
     system("pause");
 
     return 0;
 }
 
-void Blacklist_HWID(){}
+void Blacklist_HWID() {}
